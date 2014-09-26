53cc367a95cb9c2c7e6fe6ba62e88d4fb2041d0b
tab <tab@localhost.localdomain>
2014-09-26 17:22:47 +0800

    test about dump file

---
+++ test.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int readfile(char *filename,int size)
+{
+	FILE *fp;
+	char ans[64];
+	if((fp=fopen(filename,"rb"))==NULL)
+	{
+		printf("wrong read file!\n");
+		return -1;
+	}
+	fgets(ans,size,fp);
+	ans[size] = '\0';
+	while(!feof(fp))
+	{
+		printf("%s",ans);
+		fgets(ans,size,fp);
+		ans[size] = '\0';
+	}
+	fclose(fp);
+	return 0;
+}
+
+int main(int argc,int argv[])
+{
+	char filename[] = "/home/tab/Desktop/crashdump.dmp";
+	int size;
+	scanf("%d",&size);
+	printf("***********************************************************************************************\n");
+	readfile(filename,size);
+	printf("***********************************************************************************************\n");
+	return 0;
+}
