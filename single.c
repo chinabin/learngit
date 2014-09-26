d7cedc5a03c57278fb5b0df3be182e8763115eb9
tab <tab@localhost.localdomain>
2014-09-26 18:24:26 +0800

    09/26/2014

---
+++ single.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int readfile(char *filename,int size)
+{
+	FILE *fp;
+    char c;
+	if((fp=fopen(filename,"rb"))==NULL)
+	{
+		printf("wrong read file!\n");
+		return -1;
+	}
+	c=fgetc(fp);
+	while(!feof(fp))
+	{
+		printf("%c",c);
+        c=fgetc(fp);
+	}
+	fclose(fp);
+	return 0;
+}
+
+int main(int argc,int argv[])
+{
+	char filename[] = "/home/tab/leargit/crashdump.dmp";
+	int size=1;
+	printf("***********************************************************************************************\n");
+	readfile(filename,size);
+	printf("***********************************************************************************************\n");
+	return 0;
+}
